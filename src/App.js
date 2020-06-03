ea04f2f94e071158930917cbd53c258bb87233ee
saqib25 <52543072+saqib25@users.noreply.github.com>
2020-06-03 17:32:55 +0500

    added files

--- src/App.js
+++ src/App.js
@@ -5,7 +5,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+          <br />
+          updated by saqib
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
